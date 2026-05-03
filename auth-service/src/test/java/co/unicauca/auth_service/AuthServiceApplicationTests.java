98545e5d1ecca6421f926a627e3cfef2abbf2611
juanmceron-arch <juanmceron@unicauca.edu.co>
2026-05-03 12:21:18 -0500

    creacion microservicio autenticacion

---
+++ auth-service/src/test/java/co/unicauca/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.unicauca.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
