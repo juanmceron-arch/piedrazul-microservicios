b1ebc1fb53bfb7efe30f8c6f6e4d0d553568dd58
juanmceron-arch <juanmceron@unicauca.edu.co>
2026-05-03 12:21:56 -0500

    creacion capa de presentacion

---
+++ frontend-desktop/src/main/java/co/unicauca/frontend/FrontendDesktop.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package co.unicauca.frontend;
+
+/**
+ *
+ * @author Juan Martin
+ */
+public class FrontendDesktop {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
