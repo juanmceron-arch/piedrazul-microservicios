c01a558a1f7ab6b1e9e1c12c6f7940c6d1149a76
juanmceron-arch <juanmceron@unicauca.edu.co>
2026-05-03 12:12:48 -0500

    creacion microservicio especialista

---
+++ especialista-service/src/main/java/co/unicauca/especialista_service/EspecialistaServiceApplication.java
@@ -0,0 +1,13 @@
+package co.unicauca.especialista_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspecialistaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EspecialistaServiceApplication.class, args);
+	}
+
+}
---
+++ especialista-service/src/test/java/co/unicauca/especialista_service/EspecialistaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.unicauca.especialista_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EspecialistaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
